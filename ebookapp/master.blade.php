e150fcf2311d6b469d6defb67a92845a57788452
danialafiqazlan <55779720+danialafiqazlan@users.noreply.github.com>
2020-12-08 00:13:58 +0800

    Add files via upload

---
+++ ebookapp/master.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Master</h1>
+    <p>This is the Master page</p>
+@endsection
---
+++ ebookapp/nav.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Nav</h1>
+    <p>This is the Nav page</p>
+@endsection
