195cf6a44d119886b2197cb53efc58c98d23eed5
danialafiqazlan <55779720+danialafiqazlan@users.noreply.github.com>
2020-12-08 00:11:17 +0800

    Delete about.blade.php

--- about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>About</h1>
-    <p>This is a eBook Sharing Application developed by Danial Afiq 1829901.<br> The purpose of this Application
-       is to display a list of eBooks available for people to pick up and read on the go.<br> Hope you find this Application
-       easy to use and helpful. Thank You!</p>
-@endsection
